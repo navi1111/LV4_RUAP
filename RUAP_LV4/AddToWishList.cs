4d786c1c59673999afe760004290df8f6d444fe9
Ivan <lukac.ivan99@gmail.com>
2021-12-06 16:22:05 +0100

    AddToWishList

---
+++ RUAP_LV4/AddToWishList.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace SeleniumTests
+{
+    [TestFixture]
+    public class AddToWishList
+    {
+        private IWebDriver driver;
+        private StringBuilder verificationErrors;
+        private string baseURL;
+        private bool acceptNextAlert = true;
+
+        [SetUp]
+        public void SetupTest()
+        {
+            driver = new FirefoxDriver();
+            baseURL = "https://www.google.com/";
+            verificationErrors = new StringBuilder();
+        }
+
+        [TearDown]
+        public void TeardownTest()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+            Assert.AreEqual("", verificationErrors.ToString());
+        }
+
+        [Test]
+        public void TheAddToWishListTest()
+        {
+            driver.Navigate().GoToUrl("http://demowebshop.tricentis.com/");
+            driver.FindElement(By.LinkText("Gift Cards")).Click();
+            driver.FindElement(By.XPath("//div[3]/div/div/div/a/img")).Click();
+            driver.FindElement(By.Id("giftcard_1_RecipientName")).Click();
+            driver.FindElement(By.Id("giftcard_1_RecipientName")).Clear();
+            driver.FindElement(By.Id("giftcard_1_RecipientName")).SendKeys("Marko");
+            driver.FindElement(By.Id("giftcard_1_RecipientEmail")).Click();
+            driver.FindElement(By.Id("giftcard_1_RecipientEmail")).Clear();
+            driver.FindElement(By.Id("giftcard_1_RecipientEmail")).SendKeys("lukac.ivan99@gmail.com");
+            driver.FindElement(By.Id("add-to-wishlist-button-1")).Click();
+        }
+        private bool IsElementPresent(By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+
+        private bool IsAlertPresent()
+        {
+            try
+            {
+                driver.SwitchTo().Alert();
+                return true;
+            }
+            catch (NoAlertPresentException)
+            {
+                return false;
+            }
+        }
+
+        private string CloseAlertAndGetItsText()
+        {
+            try
+            {
+                IAlert alert = driver.SwitchTo().Alert();
+                string alertText = alert.Text;
+                if (acceptNextAlert)
+                {
+                    alert.Accept();
+                }
+                else
+                {
+                    alert.Dismiss();
+                }
+                return alertText;
+            }
+            finally
+            {
+                acceptNextAlert = true;
+            }
+        }
+    }
+}
